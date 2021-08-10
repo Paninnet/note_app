49d77183b035835c940aed3152a7a36c7c64b44e
PaninNet <iliapanin300999@mail.ru>
2021-08-10 10:57:03 +0300

    Finished context for Alert

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -5,12 +5,12 @@ export const Alert = () => {
 
    const alert = useContext(alertContext)
    console.log(alert);
-   if(!alert.state.visible){
+   if (!alert.state.visible) {
       return null
    }
    return (
       <div class={`alert alert-${alert.state.type || 'warning'} alert-dismissible`}>
-         <strong>Holy guacamole!</strong> {alert.state.text}
+         <div><strong>Holy guacamole!</strong> {alert.state.type==='success' ? "Note was added" : "Pull the note"}</div>
          <button onClick={alert.hide} type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
          </button>
